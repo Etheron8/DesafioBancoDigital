1eb31dd77dcb52b4ca8041a87beebfb9073ff809
Etheron <40326651+Etheron8@users.noreply.github.com>
2024-01-08 09:06:11 -0300

    first commit

---
+++ src/Cliente.java
@@ -0,0 +1,13 @@
+public class Cliente {
+
+    private String name;
+
+    public String getNome() {
+        return name;
+    }
+
+    public void setNome(String name) {
+        this.name = name;
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Cliente Felipe = new Cliente();
+        Felipe.setNome("Felipe");
+
+        Conta cc = new ContaCorrente(Felipe);
+        Conta poupanca = new ContaPoupanca(Felipe);
+
+        cc.depositar(110);
+        cc.transferir(100, poupanca);
+
+        cc.imprimirExtrato();
+        poupanca.imprimirExtrato();
+    }
+}
